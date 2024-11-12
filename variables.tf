dff41acef529c92d948c0035373b86eb1d9f4a70
Jschenk64 <josephschenk.js@gmail.com>
2024-11-12 13:59:19 +0100

    Update variables.tf
    This setup externalizes variable values, making your Terraform configuration more flexible and easier to manage for different environments.

--- variables.tf
+++ variables.tf
@@ -1,59 +1,45 @@
-variable "vpc_cidr_block" {
-  description = "The CIDR block for the VPC"
-  type        = string
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
   default     = "10.150.0.0/16"
 }
 
-variable "instance_tenancy" {
-  description = "The instance tenancy for the VPC"
-  type        = string
-  default     = "default"
-}
-
-variable "enable_dns_support" {
-  description = "Whether to enable DNS support in the VPC"
-  type        = bool
-  default     = true
-}
-
-variable "enable_dns_hostnames" {
-  description = "Whether to enable DNS hostnames in the VPC"
-  type        = bool
-  default     = true
-}
-
-variable "public_subnet_cidr_blocks" {
-  description = "The CIDR blocks for the public subnets"
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for the public subnets"
   type        = list(string)
   default     = ["10.150.10.0/24", "10.150.11.0/24"]
 }
 
-variable "private_subnet_cidr_blocks" {
-  description = "The CIDR blocks for the private subnets"
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for the private subnets"
   type        = list(string)
   default     = ["10.150.12.0/24", "10.150.13.0/24"]
 }
 
 variable "availability_zones" {
-  description = "The availability zones for the subnets"
+  description = "Availability zones for the subnets"
   type        = list(string)
   default     = ["eu-central-1a", "eu-central-1b"]
 }
 
-variable "internet_gateway_name" {
-  description = "The name tag for the Internet Gateway"
-  type        = string
-  default     = "Cra-3-IGW"
+variable "region" {
+  description = "AWS region"
+  default     = "eu-central-1"
 }
 
-variable "public_route_table_name" {
-  description = "The name tag for the public route table"
-  type        = string
-  default     = "Cra-3_Pub_RT"
-}
-
-variable "private_route_table_name" {
-  description = "The name tag for the private route table"
-  type        = string
-  default     = "Cra-3_Priv_RT"
+variable "tags" {
+  description = "Tags for the resources"
+  type        = map(string)
+  default     = {
+    vpc          = "Cra-3-VPC"
+    public_sub1  = "Cra-3-Pub1"
+    public_sub2  = "Cra-3-Pub2"
+    private_sub1 = "Cra-3-Priv1"
+    private_sub2 = "Cra-3-Priv2"
+    igw          = "Cra-3-IGW"
+    pub_rt       = "Cra-3_Pub_RT"
+    priv_rt      = "Cra-3_Priv_RT"
+    sg           = "CRA-3-SG"
+    eip          = "CRA-3_EIP"
+    nat_gw       = "Cra-3_NAT_GW"
+  }
 }
