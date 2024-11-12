c85f2d618684f636e0408fd9b5450ae2c5bd79a1
Jschenk64 <josephschenk.js@gmail.com>
2024-11-12 19:29:27 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+# VPC (aws_vpc.cra_3_vpc):
+
 resource "aws_vpc" "cra_3_vpc" {
   cidr_block           = var.vpc_cidr
   instance_tenancy     = "default"
@@ -8,6 +10,8 @@ resource "aws_vpc" "cra_3_vpc" {
     Name = var.tags["vpc"]
   }
 }
+
+# Public Subnets (aws_subnet.cra_3_pub1 and aws_subnet.cra_3_pub2):
 
 resource "aws_subnet" "cra_3_pub1" {
   vpc_id               = aws_vpc.cra_3_vpc.id
@@ -31,6 +35,8 @@ resource "aws_subnet" "cra_3_pub2" {
   }
 }
 
+# Private Subnets (aws_subnet.cra_3_priv1 and aws_subnet.cra_3_priv2):
+
 resource "aws_subnet" "cra_3_priv1" {
   vpc_id            = aws_vpc.cra_3_vpc.id
   cidr_block        = var.private_subnet_cidrs[0]
@@ -51,6 +57,8 @@ resource "aws_subnet" "cra_3_priv2" {
   }
 }
 
+# Internet Gateway (aws_internet_gateway.cra_3_igw):
+
 resource "aws_internet_gateway" "cra_3_igw" {
   vpc_id = aws_vpc.cra_3_vpc.id
 
@@ -58,6 +66,8 @@ resource "aws_internet_gateway" "cra_3_igw" {
     Name = var.tags["igw"]
   }
 }
+
+# Public Route Table (aws_route_table.cra_3_pub_rt):
 
 resource "aws_route_table" "cra_3_pub_rt" {
   vpc_id = aws_vpc.cra_3_vpc.id
@@ -73,12 +83,17 @@ resource "aws_route_table" "cra_3_pub_rt" {
 
 }
 
+# Elastic IP (aws_eip.cra_3_eip):
+# Allocates an Elastic IP, which will be used by the NAT gateway.
+
 resource "aws_eip" "cra_3_eip" {
   
   tags = {
     Name = var.tags["eip"]
   }
 }
+
+# NAT Gateway (aws_nat_gateway.cra_3_nat_gw):
 
 resource "aws_nat_gateway" "cra_3_nat_gw" {
   allocation_id = aws_eip.cra_3_eip.id
@@ -88,6 +103,8 @@ resource "aws_nat_gateway" "cra_3_nat_gw" {
     Name = var.tags["nat_gw"]
   }
 }
+
+# Private Route Table (aws_route_table.cra_3_priv_rt):
 
 resource "aws_route_table" "cra_3_priv_rt" {
   vpc_id = aws_vpc.cra_3_vpc.id
@@ -101,6 +118,8 @@ resource "aws_route_table" "cra_3_priv_rt" {
     Name = var.tags["priv_rt"]
   }
 }
+
+# Route Table Associations:
 
 resource "aws_route_table_association" "cra_3_pub1" {
   subnet_id      = aws_subnet.cra_3_pub1.id
@@ -121,6 +140,9 @@ resource "aws_route_table_association" "cra_3_priv2" {
   subnet_id      = aws_subnet.cra_3_priv2.id
   route_table_id = aws_route_table.cra_3_priv_rt.id
 }
+
+# Security Group (aws_security_group.cra_3_sg):
+# Allows inbound SSH (port 22) and HTTP (port 80) traffic from any IP (0.0.0.0/0).
 
 resource "aws_security_group" "cra_3_sg" {
   name        = "Allow Cra-3_sg"
